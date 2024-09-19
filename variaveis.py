831632dfc98055df80312a05270c2889713cb0a9
Vinicius Bariane <146668696+vinisilvabariane@users.noreply.github.com>
2024-09-19 14:23:08 -0300

    Delete variaveis.py

--- variaveis.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-RAIZ = Path(__file__).parent.resolve()
-DADOS = RAIZ / "DADOS"
-ESTACOES_CSV = DADOS / "estacoes.csv"
-QUEIMADAS_CSV = DADOS / "queimadas.csv"
-CLIMA_CSV = DADOS / "clima.csv"
