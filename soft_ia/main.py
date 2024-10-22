e9938708be891cc5ff3fdb7e254ba1f0f729c5a2
Gabriel Sampaio Santos <55637901+Gabriel534@users.noreply.github.com>
2024-10-22 20:36:45 -0300

    Ajustes no índice de Angstron e resposta da IA

--- soft_ia/main.py
+++ soft_ia/main.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 from sql import inserirDados
 import datetime
+chama = 0
+fumaca = 0
 
 if __name__ == "__main__":
     os.system("cls")
@@ -11,7 +13,12 @@ if __name__ == "__main__":
     ia.impotarDados()
     ia.tratarDados()
     ia.analisarDados()
-    ia.converterEmClassificação([0, 2.5, np.inf], ["Baixa Probabilidade", "Alta probabilidade"])
+    ia.converterEmClassificação([0, 2.5, np.inf], ["Alerta: Alta probabilidade de incêndios!", "Segurança OK"])
     ia.treinarIa()
-    resultado = ia.preverIncendio(30, 99)
+    resultado = ia.preverIncendio(99, 0)
+
+    if chama == 1 and fumaca == 1: resultado = "Alerta: Chama e fumaça detectados!"
+    elif chama == 1: resultado = "Alerta: Chama detectada!"
+    elif fumaca == 1: resultado = "Alerta: Fumaça detectada!"
+
     inserirDados(99, 30, 0, 0, datetime.datetime.now(), resultado)
