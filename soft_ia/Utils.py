742a660bf71712b828fa9a54ef1f0015abd6b83f
ViniciusBariane <146668696+vinisilvabariane@users.noreply.github.com>
2025-04-16 11:27:43 -0300

    Alterações

---
+++ soft_ia/Utils.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+RAIZ = Path(__file__).parent.resolve()
+DADOS = RAIZ.parent / "datasets"
+HOST_DATABASE = "localhost"
+USER_DATABASE = "root"
+PASSWORD_DATABASE = "123"
+DATABASE = "projeto_incendio"
+PORTA_ARDUINO = "COM3"
